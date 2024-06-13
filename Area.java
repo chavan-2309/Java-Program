49ccd7ddb04b1b474e14a5b7795a850972f9a1b0
Ashwini chavan <142683206+chavan-2309@users.noreply.github.com>
2024-06-13 23:12:25 +0530

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class area
+{
+	public static void main(String str[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Radius :: ");
+		float r=sc.nextFloat();
+		float a=(22/7)*r*r;
+		System.out.println("Area Of Circle = "+a);
+	}
+}
