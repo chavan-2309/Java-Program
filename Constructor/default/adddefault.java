715e5120f34c9c1c3c9c4954833e95f6d61144b0
Ashwini chavan <142683206+chavan-2309@users.noreply.github.com>
2024-06-16 18:14:17 +0530

    Create adddefault.java

---
+++ Constructor/default/adddefault.java
@@ -0,0 +1,15 @@
+class add
+{
+	int a,b;
+	add()
+	{
+		a=9;
+		b=5;
+		int c=a+b;
+		System.out.println("Addition = "+c);
+	}
+	public static void main(String arg[])
+	{
+		add ob=new add();
+	}
+}
