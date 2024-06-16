457d3505af406d020a7bfc69d9cb38a9403cf901
Ashwini chavan <142683206+chavan-2309@users.noreply.github.com>
2024-06-16 18:19:47 +0530

    Create case.java

---
+++ string/case.java
@@ -0,0 +1,10 @@
+class string
+{
+	public static void main(String arg[])
+	{
+		String s1="rbnbcdj";
+		System.out.println("UpperCase = "+s1.toUpperCase());
+		String s2="RBNBCDJ";
+		System.out.println("UpperCase = "+s2.toLowerCase());
+	}
+}
