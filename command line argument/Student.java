8e58fd813a0f9e7e6e7c6f5897fba19aad24f1da
Ashwini chavan <142683206+chavan-2309@users.noreply.github.com>
2024-06-24 19:26:33 +0530

    Create Student.java

---
+++ command line argument/Student.java
@@ -0,0 +1,13 @@
+class demo
+{
+	public static void main(String ar[])
+	{
+		int no=Integer.parseInt(ar[0]);
+		String sname=ar[1];
+		float per=Float.parseFloat(ar[2])
+		String sub=ar[3];
+	  System.out.println("no\t\tsname\t\tper\t\tsub\t\t");
+		System.out.println(+no+"\t\t"+sname+"\t\t"+per+"\t\t"+sub+"\t\t");
+	
+	}
+}
